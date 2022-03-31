e2c8973743d9fd116c78032ff26a07bcfe4378f0
ayana bando <ayanabando@gmail.com>
2022-03-31 17:02:31 -0400

    add additional ui components

--- app/src/main/java/com/example/android/wholsum/MainActivity.kt
+++ app/src/main/java/com/example/android/wholsum/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.android.wholsum
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -13,7 +17,8 @@ import com.example.android.wholsum.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
-
+    private lateinit var searchView: SearchView
+//TODO change icon color on toolbar or revisit app theme colors
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
@@ -33,5 +38,20 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.fragment_container)
         return navController.navigateUp()
                 || super.onSupportNavigateUp()
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        searchOptions(menu)
+        return true
+    }
+
+    private fun searchOptions(menu: Menu) {
+        val search = menu.findItem(R.id.app_bar_search)
+        searchView = search.actionView as SearchView
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (searchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
     }
 }
