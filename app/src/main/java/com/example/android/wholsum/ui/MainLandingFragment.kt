aad074c58bdca4807409f71d91afdc5ea03835aa
Ayana Bando <ayanaflax@yahoo.com>
2022-04-06 12:41:33 -0400

    add images to project and xml file

--- app/src/main/java/com/example/android/wholsum/ui/MainLandingFragment.kt
+++ app/src/main/java/com/example/android/wholsum/ui/MainLandingFragment.kt
@@ -2,13 +2,13 @@ package com.example.android.wholsum.ui
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.ui.AppBarConfiguration
-import com.example.android.wholsum.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android.wholsum.databinding.FragmentMainLandingBinding
+
 
 class MainLandingFragment : Fragment() {
     private var _binding: FragmentMainLandingBinding? = null
@@ -21,5 +21,13 @@ class MainLandingFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentMainLandingBinding.inflate(inflater, container, false)
         return binding.root
+    }
+//TODO add image to top half of screen; potentially cover toolbar
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView = binding.wholsumRecyclerv
+        val horizontalLayoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.layoutManager = horizontalLayoutManager
     }
 }
