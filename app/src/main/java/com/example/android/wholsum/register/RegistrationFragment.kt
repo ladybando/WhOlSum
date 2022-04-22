f63ba3b79fb302d0b0493c72a2dde2b8e6e96cb9
Ayana Bando <ayanaflax@yahoo.com>
2022-04-22 17:21:52 -0400

    update ui and remove unnecessary code

--- app/src/main/java/com/example/android/wholsum/register/RegistrationFragment.kt
+++ app/src/main/java/com/example/android/wholsum/register/RegistrationFragment.kt
@@ -1,40 +1,24 @@
 package com.example.android.wholsum.register
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.android.wholsum.R
+import androidx.fragment.app.Fragment
+import com.example.android.wholsum.databinding.FragmentRegistrationBinding
 
 class RegistrationFragment : Fragment() {
+    private var _binding: FragmentRegistrationBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false)
+        _binding = FragmentRegistrationBinding.inflate(inflater, container, false)
+        return binding.root
     }
-//TODO add onclick listener to drawable https://stackoverflow.com/questions/33650575/click-listener-for-drawableleft-in-edittext-in-android
-  /*
-  fun showPassword(){
-  val editText = binding.tvPassword
-    editText.setOnTouchListener(new OnTouchListener() {
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        final int DRAWABLE_LEFT = 0;
-        final int DRAWABLE_TOP = 1;
-        final int DRAWABLE_RIGHT = 2;
-        final int DRAWABLE_BOTTOM = 3;
+//TODO create theme for edit texts in related xml
 
-       if(event.getRawX() <= (editText.getCompoundDrawables()[DRAWABLE_LEFT].getBounds().width()))
-        {
-              // your action here
-             return true;
-        }
-        return false;
-    }
-});
-    }*/
 }
